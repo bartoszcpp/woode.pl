3ec445bef6ee3db5b7ae66ee05f8560044568fc7
bartoszcpp <60549143+bartoszcpp@users.noreply.github.com>
2025-04-25 10:19:45 +0200

    fix: cookie banner should block page (#2)
    * fix: cookie banner should block page
    
    * fix: change info

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "cross-fetch/polyfill";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 import { AppProvider } from "../components/contex/AppContex";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../style/global.scss";
 import "../style/Header.scss";
 import "../style/Hero.scss";
@@ -26,6 +26,8 @@ const client = new ApolloClient({
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  const [bannerVisible, setBannerVisible] = useState(true);
+
   useEffect(() => {
     const script = document.createElement("script");
     script.id = "cookieyes";
@@ -35,10 +37,52 @@ export default function MyApp({ Component, pageProps }) {
       "https://cdn-cookieyes.com/client_data/4c511732de5201b48af15e9a/script.js";
     document.body.appendChild(script);
   }, []);
+
+  useEffect(() => {
+    const checkBanner = () => {
+      const el = document.getElementById("cky-consent-banner");
+      const isVisible = el && window.getComputedStyle(el).display !== "none";
+      setBannerVisible(!!isVisible);
+    };
+
+    checkBanner();
+    const interval = setInterval(checkBanner, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <AppProvider>
       <ApolloProvider client={client}>
-        <Component {...pageProps} />
+        {bannerVisible && (
+          <div
+            style={{
+              position: "fixed",
+              inset: 0,
+              backgroundColor: "rgba(0,0,0,0.4)",
+              zIndex: 9999,
+              pointerEvents: "auto",
+            }}
+          >
+            <div
+              style={{
+                color: "#fff",
+                fontSize: "18px",
+                textAlign: "center",
+                marginTop: "20%",
+                padding: "0 20px",
+              }}
+            />
+          </div>
+        )}
+        <div
+          style={{
+            pointerEvents: bannerVisible ? "none" : "auto",
+            filter: bannerVisible ? "blur(2px)" : "none",
+          }}
+        >
+          <Component {...pageProps} />
+        </div>
       </ApolloProvider>
     </AppProvider>
   );
